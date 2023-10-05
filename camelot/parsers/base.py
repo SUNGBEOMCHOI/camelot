6eeca88963d1a1474fe06f6abf01cacd1a40b48d
SungBeom-Choi <choisb3631@gmail.com>
2023-10-06 06:23:01 +0900

    edit base.py

--- camelot/parsers/base.py
+++ camelot/parsers/base.py
@@ -7,7 +7,7 @@ from ..utils import get_text_objects
 class BaseParser:
     """Defines a base parser."""
 
-    def _generate_layout(self, filename, imagename, layout_kwargs, layout=None, dim=None):
+    def _generate_layout(self, filename, layout_kwargs, imagename=None, layout=None, dim=None):
         self.filename = filename
         self.imagename = imagename
         self.layout_kwargs = layout_kwargs
@@ -20,9 +20,9 @@ class BaseParser:
         self.horizontal_text = get_text_objects(self.layout, ltype="horizontal_text")
         self.vertical_text = get_text_objects(self.layout, ltype="vertical_text")
         self.pdf_width, self.pdf_height = self.dimensions
-        if self.filename is not None:
-            self.rootname, __ = os.path.splitext(self.filename)
-            self.imagename = "".join([self.rootname, ".png"])
         if self.imagename is not None:
             self.imagename = imagename
             self.rootname = os.path.basename(imagename).split(".")[0]
+        if self.filename is not None:
+            self.rootname, __ = os.path.splitext(self.filename)
+            self.imagename = "".join([self.rootname, ".png"])
